a782367e1680cea00164966f3b282ff169f88f33
Nima Garshasebi <67752604+nimagarshasebi@users.noreply.github.com>
2024-12-28 21:46:58 +0330

    original

--- ultralytics/models/yolo/model.py
+++ ultralytics/models/yolo/model.py
@@ -109,3 +109,5 @@ class YOLOWorld(Model):
         # self.predictor = None  # reset predictor otherwise old names remain
         if self.predictor:
             self.predictor.model.names = classes
+model = YOLO("yolo11x-obb.yaml")
+print(model)
